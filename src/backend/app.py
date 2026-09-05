acd73e66718d66a55246cde3e2860590340d852e
manojitkhatua <manojitkhatua8@gmail.com>
2026-09-05 11:31:02 +0530

    Prepare backend for deployment

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import sys
 
+import os
 import pandas as pd
 from flask import Flask, jsonify, request
 from flask_cors import CORS
@@ -172,8 +173,10 @@ def predict():
 # ============================================================
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
         host="0.0.0.0",
-        port=5000,
-        debug=True,
+        port=port,
+        debug=False,
     )
